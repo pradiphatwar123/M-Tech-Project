f8233c268d2f8dfb5ac1b503841bb5c92f5130d8
pradiphatwar123 <pradiphatwar123@gmail.com>
2014-09-17 11:22:11 +0530

    Create Node.cpp

---
+++ Node.cpp
@@ -0,0 +1,181 @@
+#include "project.h"
+
+
+
+Node node;
+
+/*******
+
+class Node
+{
+	private :
+
+		unsigned int time;
+		unsigned int node_id;
+		
+		struct Connection_Queue
+		{
+			Node *from;
+			Node *to;
+			unsigned int time;
+		}Node=NULL;
+
+		
+		struct Message_Queue
+		{
+			Node *from;
+			char *message;
+			unsigned int time;
+		}Node=NULL;
+
+		void Send_Message( Node *target, int Message_Type, char *Message);
+
+		void Receive_Message( Node *from, int Message_Type, char *Message );
+
+		void Process_Received_Message( Node *origin , int Message_Type, char *Message );
+
+		void Broadcast_Message_To_Neighbours( char *Message, int Message_Type );		
+	
+		void Broadcast_Message_To_All_Nodes( char *Message, int Message_Type );	
+
+		void Find_Neighbours( int Node_Id);
+
+		int IsNeighbour( Node *xyzNode);
+
+		struct Remove_Message_From_Queue
+		{
+			Node *to;
+			unsigned int time;
+		};
+		
+};
+
+****/
+
+/*********************************************************************************************************
+Function Name            : 
+
+Inputs                   : 
+
+Outputs                  : 
+
+Description              :  
+*********************************************************************************************************/
+
+void Send_Message( Node *target, int Message_Type, char *Message)
+{
+
+
+
+
+}
+
+
+/*********************************************************************************************************
+Function Name            : 
+
+Inputs                   : 
+
+Outputs                  : 
+
+Description              :  
+*********************************************************************************************************/
+
+void Receive_Message( Node *from, int Message_Type, char *Message )
+{
+
+
+
+}
+
+/*********************************************************************************************************
+Function Name            : 
+
+Inputs                   : 
+
+Outputs                  : 
+
+Description              :  
+*********************************************************************************************************/
+
+void Process_Received_Message( Node *origin , int Message_Type, char *Message )
+{
+
+
+
+
+}
+
+/*********************************************************************************************************
+Function Name            : 
+
+Inputs                   : 
+
+Outputs                  : 
+
+Description              :  
+*********************************************************************************************************/
+
+void Broadcast_Message_To_Neighbours( char *Message, int Message_Type )
+{
+
+
+
+
+}
+
+/*********************************************************************************************************
+Function Name            : 
+
+Inputs                   : 
+
+Outputs                  : 
+
+Description              :  
+*********************************************************************************************************/
+
+void Broadcast_Message_To_All_Nodes( char *Message, int Message_Type )
+{
+
+
+
+
+}
+
+/*********************************************************************************************************
+Function Name            : 
+
+Inputs                   : 
+
+Outputs                  : 
+
+Description              :  
+*********************************************************************************************************/
+
+void Find_Neighbours( int Node_Id)
+{
+
+
+
+
+}
+
+/*********************************************************************************************************
+Function Name            : 
+
+Inputs                   : 
+
+Outputs                  : 
+
+Description              :  
+*********************************************************************************************************/
+
+
+int IsNeighbour( Node *xyzNode)
+{
+	int error = 0 ;
+
+	return 0;
+
+}
+

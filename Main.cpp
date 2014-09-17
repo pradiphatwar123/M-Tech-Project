6259477f7e6a1ec15dafef0c272cc000d5568017
pradiphatwar123 <pradiphatwar123@gmail.com>
2014-09-17 11:21:50 +0530

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,167 @@
+/*********************************************************************************************************
+
+Project           :  Synchronous Distributed System
+
+File name         :  main.CPP
+
+Description       :  
+
+Programmers       :  (1) Pradip Hatwar     (CS13M022)
+                     (2) Sachin Balchim    (CS13M0 )
+                     (3) Bibekananda Raul  (CS13M0 )
+
+Compiler name     :  
+
+Date created      :  
+
+List of functions : (1)  int 	main(int argc, char *argv[])
+		    (2)  int	ValidateCmdline();
+		    (3)	 int	ValidateFile();	
+		    (4)  int 	ValidateInput();
+		    (5)  void	ReadInputs();	
+		    (6)  int	WriteOutput();	
+                    
+History           :  
+
+*********************************************************************************************************/
+
+
+#include "project.h"
+
+
+
+
+/********
+
+#include<iostream>
+#include<cstdlib>
+#include<ctype.h>
+#include<malloc.h>
+#include<fstream>
+#include<string.h>
+#include<math.h>
+#include<cstdlib.h>
+
+ 
+
+int 	ValidateCmdline();
+int 	ValidateFile();
+int 	ValidateInput();
+void	ReadInputs();
+int 	WriteOutput();
+
+******/
+
+Distributed_System DS;
+	Node node;
+
+
+int main()
+{
+	
+   
+	ValidateCmdline();
+ 	
+	ValidateFile();
+ 	
+	ValidateInput();
+	
+	ReadInputs();
+ 	
+		
+	WriteOutput();
+	
+	return 0;
+}
+
+
+
+/*********************************************************************************************************
+Function Name            : WriteOutput
+
+Inputs                   : 
+
+Outputs                  : 
+
+Description              :  
+*******************************************************************************************************/
+int Validate_Cmdline()
+{
+	int 	error=0;
+	
+	return error;        
+}
+
+/*********************************************************************************************************
+Function Name            : 
+
+Inputs                   : 
+
+Outputs                  : 
+
+Description              :  
+*******************************************************************************************************/	
+
+int ValidateFile()
+{
+
+
+
+
+}
+
+/*********************************************************************************************************
+Function Name            : 
+
+Inputs                   : 
+
+Outputs                  : 
+
+Description              :  
+*******************************************************************************************************/
+
+int ValidateInput()
+{
+	int error =0 ;
+
+	
+	return error;
+}
+
+
+/*********************************************************************************************************
+Function Name            : 
+
+Inputs                   : 
+
+Outputs                  : 
+
+Description              :  
+*********************************************************************************************************/
+
+void ReadInputs()
+{
+
+
+	
+
+}
+
+/*********************************************************************************************************
+Function Name            : WriteOutput
+
+Inputs                   : 
+
+Outputs                  : 
+
+Description              :  
+*********************************************************************************************************/
+
+int WriteOutput()
+{
+	int error =0 ;
+
+	
+	return error;
+
+}
